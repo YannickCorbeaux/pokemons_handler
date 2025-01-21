cbc3ff7e86b265e9a9b65ea4e7051ddb7535cc30
root <root@YayaPC.>
2025-01-21 02:48:26 +0100

    fix res.json as res.send with template string

--- app/controllers/pokemonsController.js
+++ app/controllers/pokemonsController.js
@@ -3,16 +3,26 @@ import pokemonsDataMapper from '../dataMapper/pokemonsDataMapper.js';
 // function to get all Pokemons from my json (no db at this time)
 export async function getAllPokemons(req, res) {
   const pokemons = await pokemonsDataMapper.getAllPokemons();
-  res.json(pokemons);
+  // send a json with a message contains number of pokemons and the list of their pokemons
+  res.json({
+    message: `Il y a ${pokemons.length} pokemons dans le pokedex, les voici : `,
+    data: pokemons,
+  });
 }
 
 // function to get one Pokemon by id
 export async function getOnePokemon(req, res) {
   try {
-    const pokemonId = req.pokemonId; // Use validate id from the middleware
+    // Use validate id from the middleware
+    const pokemonId = req.pokemonId;
+    // Get the pokemon by id
     const pokemon = await pokemonsDataMapper.getOnePokemon(pokemonId);
     if (pokemon) {
-      res.json(pokemon);
+      res.send(`Tu as sélectionné le pokémon ${pokemon.name}, son id est le ${pokemon.id}, il a ${pokemon.hp} points de vie et ${pokemon.cp} points de caractéristiques, enfin il est de type ${pokemon.types[0]}${pokemon.types[1] ? ` et ${pokemon.types[1]}` : ''}.<br>Voici à quoi il ressemble : <br><img src="${pokemon.picture}" alt="${pokemon.name}"><br> Magnifique n'est-ce pas ? 💩`);
+      // res.json({
+      //   message: `Tu as sélectionné le pokémon ${pokemon.name}, voici ses données :`,
+      //   data: pokemon,
+      // });
     }
     else {
       res.status(404).send('Pokemon not found');
